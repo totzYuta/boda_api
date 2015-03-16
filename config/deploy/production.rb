f5287f9bfd80899e87aacb6d691ad77326d6f25b
totz <yuta.totz@gmail.com>
2015-03-15 22:25:21 -0700

    [modify] Modify config file for new VM

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,13 +8,13 @@ set :deploy_user, "totz"
 # the 'full name' of the application
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 # the server(s) to deploy to
-server 'bodaapivm.cloudapp.net', user: 'totz', roles: %w{web app db}, primary: true
+server 'bodav.cloudapp.net', user: 'totz', roles: %w{web app db}, primary: true
 # the path to deploy to
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 # set to production for Rails
 set :rails_env, :production
 
-Rake::Task["nginx:reload"].clear_actions
+# Rake::Task["nginx:reload"].clear_actions
 
 
 # server-based syntax
