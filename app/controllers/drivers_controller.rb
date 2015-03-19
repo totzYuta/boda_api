b730c0d3cb2cd401fd8a0dd290492288c5f06270
totz <yuta.totz@gmail.com>
2015-03-18 21:16:00 -0700

    [fix] Fix bug of history of drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -15,7 +15,7 @@ class DriversController < ApplicationController
   
       # History of Drivers current_user used
       if !History.find_by_user_id(current_user.id).nil?
-        histories = History.all
+        histories = History.where(user_id: current_user.id)
         @his_drivers = []
         histories.each do |his|
           @his_drivers << Driver.find(his.driver_id) 
